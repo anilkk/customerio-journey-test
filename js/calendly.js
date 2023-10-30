7197e38eea34730259722bad39403c809528da5b
Anil Kumar <anilbms75@gmail.com>
2023-10-30 18:30:11 +0100

    Add tracking to calendar event

---
+++ js/calendly.js
@@ -0,0 +1,34 @@
+function isCalendlyScheduledEvent(e) {
+    return e.data.event &&
+           e.data.event.indexOf('calendly.event_scheduled') === 0;
+}
+
+window.addEventListener(
+    'message',
+    function(e) {
+        if (isCalendlyScheduledEvent(e)) {
+            console.log('Calendly data', e.data);
+            var inviteeUri = e.data.payload.invitee.uri;
+            var eventId = inviteeUri.split('/')[4];
+            var inviteeId = inviteeUri.split('/')[6];
+
+            // write a code to ftech data from backend api
+            // and update the event with the data
+            $.ajax({
+                type: 'GET',
+                url: `https://manage-calendly-event.vercel.app/api/getinviteeinfo?eventId=${eventId}&inviteeId=${inviteeId}`,
+                success: function(data) {
+                    console.log(data);
+                    var userEmail = data.email;
+                    if (userEmail) {
+                            // update the event with the data    
+                            analytics.identify(userEmail, {
+                                email: userEmail
+                            });
+                            console.log("addUserEmail");
+                        }
+                    }
+            });
+        }
+    }
+);
